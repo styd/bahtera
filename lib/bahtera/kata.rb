41ad9e153d7063deefc14ef5b993b3844f1bbe44
Adinda Praditya <apraditya@gmail.com>
2013-04-22 02:24:21 +0800

    Made Kata attributes that are array or hash, as their own class

--- lib/bahtera/kata.rb
+++ lib/bahtera/kata.rb
@@ -1,21 +1,65 @@
 module Bahtera
-  class Kata
+  class BaseKata
     def initialize(hash)
-      @hash_response = hash['kateglo']
-      assign_attribute_names
+      assign_attribute_names hash
     end
 
 
     private
-      def assign_attribute_names
-        attributes = @hash_response.keys
+      def assign_attribute_names(hash_response)
+        attributes = hash_response.keys
         self.class.class_eval { attr_reader *attributes }
         attributes.each do |attr_name|
-          if @hash_response[attr_name]
+          if hash_response[attr_name]
             instance_variable_set("@#{attr_name}",
-                                    @hash_response[attr_name])
+                                    hash_response[attr_name])
           end
         end
+      end
+  end
+
+  class Kata < BaseKata
+    def initialize(hash)
+      assign_attribute_names hash['kateglo']
+    end
+
+
+    private
+      def assign_attribute_names(hash_response)
+        super hash_response
+        %w( root definition reference proverbs translations all_relation ).each do |attribute_name|
+          override_array_hash(attribute_name)
+          self.class.send :define_method, "has_#{attribute_name}?" do
+            send(attribute_name).any?
+          end
+        end
+        assign_relations if relation.any?
+      end
+
+      def assign_relations
+        relation_hashes = relation.keys.map do |rel_type|
+          relation[rel_type] if relation[rel_type].is_a?(Hash)
+        end.compact
+        relation_hashes.each do |relation_hash|
+          if relation_hash['name']
+            method_name = relation_hash.delete('name').downcase.gsub(/\s/, '_')
+            self.class.send :define_method, method_name.to_sym do
+              relation_hash.values.map do |hash_attr|
+                (hash_attr.is_a?(Hash))? BaseKata.new(hash_attr) : hash_attr
+              end
+            end
+            self.class.send :define_method, "has_#{method_name}?" do
+              self.send(method_name).any?
+            end
+          end
+        end
+      end
+
+      def override_array_hash(attr_name)
+        new_value = send(attr_name).map do |hash_attr|
+          BaseKata.new(hash_attr)
+        end
+        instance_variable_set("@#{attr_name}", new_value)
       end
   end
 end
--- spec/lib/bahtera/kata_spec.rb
+++ spec/lib/bahtera/kata_spec.rb
@@ -19,9 +19,39 @@ describe Bahtera::Kata do
         @kata = Bahtera::Kata.new(parsed_response)
       end
 
+      array_attributes = {}
       parsed_response['kateglo'].keys.each do |attr_name|
+        if parsed_response['kateglo'][attr_name].is_a?(Array)
+          array_attributes[attr_name] = parsed_response['kateglo'][attr_name]
+          next
+        end
         it "should assign ##{attr_name} correctly" do
           @kata.send(attr_name).should == @expected_attributes[attr_name]
+        end
+      end
+
+      array_attributes.each do |attr_name, array|
+        it "##{attr_name} should transform to the array of Bahtera::BaseKata" do
+          @kata.send(attr_name).all? do |entry|
+            entry.is_a?(Bahtera::BaseKata)
+          end.should be_true
+        end
+
+        it "should respond_to #has_#{attr_name}?" do
+          method_name = "has_#{attr_name}?"
+          @kata.should respond_to(method_name)
+          @kata.send(method_name).should == @expected_attributes[attr_name].any?
+        end
+      end
+
+      describe 'assigning relation methods' do
+        %w( sinonim antonim berkaitan turunan gabungan_kata peribahasa).each do |attr_name|
+          has_method_name = "has_#{attr_name}?"
+
+          it "should respond to ##{attr_name} & ##{has_method_name}" do
+            @kata.should respond_to(attr_name)
+            @kata.should respond_to(has_method_name)
+          end
         end
       end
     end
